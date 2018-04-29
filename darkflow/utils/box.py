ee5354f8054fb5d26c8afb4bfb93f5cf3c4ccfba
andejoha <anders.johannessen@lyse.net>
2018-04-29 10:23:02 +0200

    Fixing plot

--- darkflow/utils/box.py
+++ darkflow/utils/box.py
@@ -21,8 +21,8 @@ def evaluate_bounding_boxes(annotation_boxes):
     n = 0
     for true_box in annotation_boxes:
         for predicted_box in predicted_boxes:
-            print(true_box.name, predicted_box.name)
             if true_box.name == predicted_box.name:
+                print(true_box.name, predicted_box.name)
                 temp_iou = box_iou(true_box, predicted_box)
                 if temp_iou > 0:
                     iou += temp_iou
